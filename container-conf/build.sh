4fb9d93522f934c96ba6a6972f7fcd36692f5fa2
rorour <ravenmarieorourke@gmail.com>
2022-06-10 22:59:07 +0000

    simplify build

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -4,10 +4,7 @@ build_is_public=1
 
 build_as_root() {
     umask 022
-    build_yum config-manager \
-        --add-repo \
-        https://download.docker.com/linux/fedora/docker-ce.repo
     build_yum install fedora-workstation-repositories
     build_yum config-manager --set-enabled google-chrome
-    build_yum install google-chrome-stable docker-ce-cli
+    build_yum install google-chrome-stable
 }
