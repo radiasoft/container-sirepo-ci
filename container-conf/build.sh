d4634fd4e268b8896dfa562bc16c7728a8201e7f
rorour <ravenmarieorourke@gmail.com>
2022-06-16 17:40:21 +0000

    fix #4: Add karma globally and fix test.sh

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -8,3 +8,15 @@ build_as_root() {
     build_yum config-manager --set-enabled google-chrome
     build_yum install google-chrome-stable
 }
+
+build_as_run_user() {
+    umask 022
+    cd ~
+    git clone https://github.com/radiasoft/sirepo.git
+    cd sirepo
+    declare -a d=($(jq -r '.devDependencies | keys |  @sh' package.json))
+    for n in "${d[@]}"
+    do
+      npm install -g $(eval n=$n && echo $n)
+    done
+}
