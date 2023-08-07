aa62d30e54f5a4db6aa04d9d38594851ce7fe0cd
e-carlin <evan@carlin.com>
2023-08-07 19:58:33 +0000

    WIP radiasoft/container-sirepo-devbox#7: Move common dependencies into container-sirepo-base installer
    Both container-sirepo-devbox and container-sirepo-ci need some common
    dependencies so move them into an installer and call it.

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -3,15 +3,9 @@ build_image_base=${sirepo_ci_base:-radiasoft/beamsim}
 build_is_public=1
 
 build_as_root() {
-    umask 022
-    build_yum install fedora-workstation-repositories
-    build_yum config-manager --set-enabled google-chrome
-    build_yum install google-chrome-stable
+    install_repo_eval container-sirepo-base root
 }
 
 build_as_run_user() {
-    install_url radiasoft/sirepo
-    #POSIT: This relies on the fact that individual package names don't have spaces or special chars
-    npm install -g \
-        $(install_download package.json | jq -r '.devDependencies | to_entries | map("\(.key)@\(.value)") | .[]')
+    install_repo_eval container-sirepo-base
 }
